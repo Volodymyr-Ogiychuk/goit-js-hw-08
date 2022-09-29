9fefc98b9dbbe37ff031fe97c7bfa30d2d3fe79a
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2022-09-29 14:30:40 +0300

    add updated feddback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,22 +5,13 @@ import throttle from 'lodash.throttle';
 const formRef = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+
 let userData = {
   
   email: formRef.elements.email.value,
   message: formRef.elements.message.value
 };
 
-initPage();
-
-const handleInput = event => {
-  const { name, value } = event.target;
-
-  userData[name] = value;
-
-  const strData = JSON.stringify(userData);
-  localStorage.setItem(LOCALSTORAGE_KEY, strData);
-};
 
 function initPage() {
   let savedData = localStorage.getItem(LOCALSTORAGE_KEY);
@@ -38,28 +29,98 @@ function initPage() {
   }
 }
 
+
+initPage();
+
+const handleInput = event => {
+  const { name, value } = event.target;
+    
+  userData[name] = value;
+
+  const strData = JSON.stringify(userData);
+  localStorage.setItem(LOCALSTORAGE_KEY, strData);
+  console.log("strData", strData);
+};
+
+
 const handleSubmit = event => {
   event.preventDefault();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
   const {
     elements: { email, message },
   } = event.currentTarget;
+ 
 
   if (email.value === '' || message.value === '') {
+    alert('Fill all the fields!');
     return;
   }
-  userData = {};
+
   const formData = new FormData(event.currentTarget);
 
   formData.forEach((value, name) => {
     userData[name] = value;
   });
   console.log(userData);
-  localStorage.removeItem(LOCALSTORAGE_KEY);
   event.currentTarget.reset();
+  userData = { email: "", message: ""};
 
 };
 
 const throttledHandleInput = throttle(handleInput, 500);
-
-formRef.addEventListener('submit', handleSubmit);
 formRef.addEventListener('input', throttledHandleInput);
+formRef.addEventListener('submit', handleSubmit);
+
+
+
+
+// 'use strict';
+// import throttle from 'lodash.throttle';
+
+// const formRef = document.querySelector('.feedback-form');
+// const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+// let userData = {
+  
+//   email: formRef.elements.email.value,
+//   message: formRef.elements.message.value,
+// };
+
+// let savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+
+//   if (savedData) {
+//     userData = JSON.parse(savedData);
+//     formRef.elements.email.value = userData.email;
+//     formRef.elements.message.value = userData.message;
+    
+//   }
+
+// let strData = {};
+
+// formRef.addEventListener('input', throttle(event => {
+  
+//   const { name, value } = event.target;
+//   userData[name] = value;
+//   strData = JSON.stringify(userData);
+//   localStorage.setItem(LOCALSTORAGE_KEY, strData);
+  
+// }, 500) );
+  
+  
+// formRef.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const {
+//     elements: { email, message },
+//   } = event.currentTarget;
+
+//   if (email.value === '' || message.value === '') {
+//     alert('Заповніть всі поля!');
+//     return;
+//   }
+//   console.log(userData);
+//   localStorage.removeItem(LOCALSTORAGE_KEY);
+//   event.currentTarget.reset();
+//   userData = {email: "", message: ""};
+
+// });
+
